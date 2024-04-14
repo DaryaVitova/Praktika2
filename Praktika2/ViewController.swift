31e5f69070dcd8a6229fe8735a8b25ddcf010426
HelloWord <d.vitova@yandex.ru>
2024-04-14 13:45:15 +0300

    Initial Commit

---
+++ Praktika2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Praktika2
+//
+//  Created by Дарья on 14.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
